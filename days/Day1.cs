d6430ad0f7d7ee21b52f8810114408f3ad146508
VictorVictini <84572057+VictorVictini@users.noreply.github.com>
2025-12-01 21:46:51 +0000

    day 1 solution

--- days/Day1.cs
+++ days/Day1.cs
@@ -1,15 +1,58 @@
 namespace AdventOfCode2025 {
     public class Day1 : Day<int> {
-        public Day1() { }
-        private void ReadFile() {
+        private struct Instruction {
+            public int direction, distance;
+            public Instruction(bool isRight, int distance) {
+                this.direction = isRight ? 1 : -1;
+                this.distance = distance;
+            }
+        }
+        private Instruction[] instructions;
+        public Day1() {
+            instructions = ReadFile();
+        }
+        private Instruction[] ReadFile() {
             string[] lines = File.ReadAllLines("inputs/day1.txt");
-            return;
+            Instruction[] inst = new Instruction[lines.Length];
+            for (int i = 0; i < lines.Length; i++) {
+                string line = lines[i];
+                inst[i] = new Instruction(line[0] == 'R', Convert.ToInt32(line.Substring(1)));
+            }
+            return inst;
         }
         public override int Part1() {
-            return -1;
+            int INITIAL_VALUE = 50;
+            int UPPER_LIMIT = 100;
+
+            int dial = INITIAL_VALUE;
+            int count = 0;
+            foreach (Instruction inst in instructions) {
+                // apply instruction
+                dial += inst.direction * inst.distance;
+                dial %= UPPER_LIMIT;
+                while (dial < 0) dial += UPPER_LIMIT;
+
+                // check value
+                if (dial == 0) count++;
+            }
+            return count;
         }
         public override int Part2() {
-            return -1;
+            int INITIAL_VALUE = 50;
+            int UPPER_LIMIT = 100;
+
+            // brute force simulation
+            int dial = INITIAL_VALUE;
+            int count = 0;
+            foreach (Instruction inst in instructions) {
+                for (int i = 0; i < inst.distance; i++) {
+                    dial += inst.direction;
+                    dial %= UPPER_LIMIT;
+                    if (dial < 0) dial += UPPER_LIMIT;
+                    if (dial == 0) count++;
+                }
+            }
+            return count;
         }
     }
 }
